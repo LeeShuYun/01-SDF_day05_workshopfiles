be3fe276d6bdae5f54b66246eb3d3a0f732fc8f3
LeeShuyun <quaternion.design@gmail.com>
2022-12-09 12:03:09 +0800

    read and print 1st 20 lines in console

---
+++ src/milton/Main.java
@@ -0,0 +1,34 @@
+package milton;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+//java -cp classes milton.Main paradise_lost.txt
+public class Main {
+    public static void main(String[] args){
+        //read file name
+        String fileName = args[0];
+        System.out.printf("Processing %s\n", fileName);
+
+        try{
+            File fileObj = Paths.get("src/milton/" + fileName).toFile();
+            FileReader fileReader = new FileReader(fileObj);
+            BufferedReader bdf = new BufferedReader(fileReader);
+
+            //read the lines up to line 20 
+            String line;
+            Integer lineCount = 0;
+            while ((line = bdf.readLine()) != null &&  lineCount <= 20 ){
+                System.out.println("> " + line);
+                lineCount++;
+            }
+            bdf.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
